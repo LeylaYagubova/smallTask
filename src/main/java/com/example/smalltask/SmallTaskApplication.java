a57a4d634dd007c69a2be1c4eb459e93ac8a288c
Leyla <leylaali130@gmail.com>
2021-10-26 07:17:23 -0700

    add default files

---
+++ src/main/java/com/example/smalltask/SmallTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.smalltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallTaskApplication.class, args);
+    }
+
+}
